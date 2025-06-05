f1ad59b2b2ce2966af27adc1a27c196c6c29b87d
dakcoh <165531758+dakcoh@users.noreply.github.com>
2025-06-05 13:37:17 +0900

    [Silver V] Title: 돌 게임, Time: 108 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String win = "CY";
+
+        while (N > 0) {
+            if (N >= 3) {
+                N = N - 3;
+                win = win.equals("SK") ? "CY" : "SK";
+            } else {
+                N = N - 1;
+                win = win.equals("SK") ? "CY" : "SK";
+            }
+        }
+        System.out.println(win);
+    }
+}
