d3f247e1f44ed8b0339b3cc3ef8145ace1cc2b4a
dakcoh <165531758+dakcoh@users.noreply.github.com>
2025-09-24 10:37:24 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        int save = n * m;
+        
+        while (m != 0) {
+            int temp = m;
+            m = n % m;
+            n = temp;
+        }
+        
+        answer[0] = n;
+        answer[1] = save / n;
+        return answer;
+    }
+}
